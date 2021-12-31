4e17e4d313fae33a7cfe4b8e35ad237c30e55706
lukasz_dyminski <lukasz.dyminski78@gmail,com>
2021-12-31 21:19:00 +0100

    Indexing Index template 2

---
+++ lulublog/templatetags/pagination_extras.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def pagination_reverse_numbering(page_obj, loop_count):
+    return page_obj.paginator.count - page_obj.start_index() - loop_count + 1
