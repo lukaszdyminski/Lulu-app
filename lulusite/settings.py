cbd0bada0c9830007462b3bb8b43b0f25decc7db
lukasz_dyminski <lukasz.dyminski78@gmail.com>
2022-01-16 22:32:08 +0100

    AWS S3 5

--- lulusite/settings.py
+++ lulusite/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'lulu_media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'lulu_images')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'dashboard'
