437995d83eb8f9001253bf3f73089b461c7b4ee7
lukasz_dyminski <lukasz.dyminski78@gmail.com>
2022-01-17 13:49:33 +0100

    AWS S3 11

--- lulusite/settings.py
+++ lulusite/settings.py
@@ -162,6 +162,8 @@ ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 2
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'lulu-app-bucket'
@@ -169,7 +171,8 @@ AWS_STORAGE_BUCKET_NAME = 'lulu-app-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = "eu-central-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 if AWS_STORAGE_BUCKET_NAME:
     MEDIA_URL = 's3://lulu-app-bucket/lulu_images/'
