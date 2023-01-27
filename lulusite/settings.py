a4017c2b4dee69cb00f487b9c9c8ac02e2d502cd
lukaszdyminski <lukasz.dyminski78@gmail.com>
2023-01-28 00:02:42 +0100

    Update lulusite settings

--- lulusite/settings.py
+++ lulusite/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+
 ALLOWED_HOSTS = ["127.0.0.1", "lulu-stories.herokuapp.com"]
 
 AUTHENTICATION_BACKENDS = [
